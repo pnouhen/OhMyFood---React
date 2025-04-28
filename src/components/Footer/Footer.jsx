d8b5b53ff28e43f861d4b28b18a71b0b97cd42d4
Pierre Nouhen <pierre.nouhen@orange.fr>
2025-04-28 13:05:30 +0200

    Modification des images header et footer + vercel.json pour gérer le rafraichissement de la page menu

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,17 +1,27 @@
-import "./footer.scss"
+import "./footer.scss";
 
-export default function Footer(){
-    return(
-        <footer>
-        <ul>
-          <li><i className="fa-solid fa-utensils"></i>Proposer un restaurant</li>
-          <li><i className="fa-solid fa-handshake-angle"></i>Devenir partenaire</li>
-        </ul>
-        <ul>
-          <li>Mentions légales</li>
-          <li><a href="mailto:example1@exemple.com">Contact</a></li>
-        </ul>
-        <img src="/assets/images/logo/ohmyfood.webp" alt="Logo website" />
-      </footer> 
-    )
+export default function Footer() {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <i className="fa-solid fa-utensils"></i>Proposer un restaurant
+        </li>
+        <li>
+          <i className="fa-solid fa-handshake-angle"></i>Devenir partenaire
+        </li>
+      </ul>
+      <ul>
+        <li>Mentions légales</li>
+        <li>
+          <a href="mailto:example1@exemple.com">Contact</a>
+        </li>
+      </ul>
+      <img
+        src="/assets/images/logo/ohmyfood.webp"
+        alt="Logo website"
+        loading="lazy"
+      />
+    </footer>
+  );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,16 +2,17 @@ import { NavLink } from "react-router";
 
 import "./header.scss";
 
-export default function Header({arrowLeft}) {
+export default function Header({ arrowLeft }) {
   return (
     <header>
       <NavLink to="/">
-      <i className={arrowLeft}></i>
+        <i className={arrowLeft}></i>
       </NavLink>
       <img
         className="logo"
         src="/assets/images/logo/ohmyfood.webp"
         alt="Logo website"
+        loading="lazy"
       />
     </header>
   );
